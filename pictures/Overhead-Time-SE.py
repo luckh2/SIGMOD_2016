98fc99c9d59657e9adb94653d7966451de4393a4
Zhao Zhang <zhangzhao0718@gmail.com>
2015-11-18 21:13:17 -0800

    update figures

--- pictures/Overhead-Time-SE.py
+++ pictures/Overhead-Time-SE.py
@@ -26,7 +26,7 @@ ax.set_xlabel('Scale (Number of Machines)', size='x-large')
 ax.set_ylabel('Wall Clock Time (seconds)', size='x-large', labelpad=20)
 ax.set_xticks(ind+0.4)
 ax.set_xticklabels( ('8', '16', '32', '64'), size='x-large') 
-ax.legend( (rects1[0], rects2[0]), ('SE Meta-only', 'SE Meta+Data'), loc='best', ncol=2)
+ax.legend( (rects1[0], rects2[0]), ('SE Meta-only', 'SE Meta+Data'), loc='best')
 ax.set_yticklabels((0, 100, 200, 300, 400, 500), size='large')
 
 
